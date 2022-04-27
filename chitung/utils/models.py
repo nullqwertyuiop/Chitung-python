ca6a649d0716bda99fd779779f0ff9395323016a
nullqwertyuiop <49110142+nullqwertyuiop@users.noreply.github.com>
2022-04-27 18:07:21 +0800

    Create modesl

---
+++ chitung/utils/models.py
@@ -0,0 +1,69 @@
+from typing import List, Literal
+
+from pydantic import BaseModel
+
+
+class FunctionControl(BaseModel):
+    fish: bool
+    casino: bool
+    responder: bool
+    lottery: bool
+    game: bool
+
+
+class RequestControl(BaseModel):
+    answerFriend: bool
+    answerGroup: bool
+    addFriend: bool
+    addGroup: bool
+    autoAnswer: bool
+
+
+class CommonControl(BaseModel):
+    joinGroupText: str
+    rejectGroupText: str
+    onlineText: str
+    welcomeText: str
+    permissionChangedText: str
+    groupNameChangedText: str
+    nudgeText: str
+
+
+class Config(BaseModel):
+    botName: str
+    devGroupID: List[int]
+    adminID: List[int]
+    minimumMembers: int
+    friendFC: FunctionControl
+    groupFC: FunctionControl
+    rc: RequestControl
+    cc: CommonControl
+
+
+class GroupConfig(BaseModel):
+    groupID: int
+    globalControl: bool
+    fish: bool
+    casino: bool
+    responder: bool
+    lottery: bool
+    game: bool
+    blockedUser: List[int]
+
+
+class GroupConfigList(BaseModel):
+    groupConfigList: List[GroupConfig]
+
+
+class UniversalRespond(BaseModel):
+    messageKind: Literal["Friend", "Group", "Any"]
+    listResponseKind: Literal["Friend", "Group"]
+    listKind: Literal["Black", "White"]
+    userList: List[int]
+    triggerKind: Literal["Equal", "Contain"]
+    pattern: List[str]
+    answer: List[str]
+
+
+class UniversalRespondList(BaseModel):
+    universalRespondList: List[UniversalRespond]
