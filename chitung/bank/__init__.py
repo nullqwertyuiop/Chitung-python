14e03824c287aada8cd295ce890df7b48a4b3e91
nullqwertyuiop <49110142+nullqwertyuiop@users.noreply.github.com>
2022-04-27 18:08:43 +0800

    Update bank
    1. Add /set
    2. Add /laundry

--- chitung/bank/__init__.py
+++ chitung/bank/__init__.py
@@ -3,21 +3,23 @@ import os
 import os.path
 from enum import Enum
 from pathlib import Path
-from typing import Union, List
+from typing import Union, List, Dict, NoReturn
 
 from graia.ariadne.app import Ariadne
 from graia.ariadne.event.message import GroupMessage, MessageEvent
 from graia.ariadne.message.chain import MessageChain
 from graia.ariadne.message.element import At, Plain
-from graia.ariadne.message.parser.twilight import Twilight, FullMatch
+from graia.ariadne.message.parser.twilight import Twilight, FullMatch, RegexMatch, RegexResult
 from graia.ariadne.model import Member
 from graia.saya import Channel
 from graia.saya.builtins.broadcast import ListenerSchema
 
+from chitung.utils.config import config
+
 channel = Channel.current()
 
 channel.name("ChitungBank")
-channel.author("角川烈、白门守望者")
+channel.author("角川烈&白门守望者 (Chitung-public)，nullqwertyuiop (Chitung-python)")
 channel.description("/bank")
 
 data_dir = Path(Path(__file__).parent / "data")
@@ -32,6 +34,8 @@ class Currency(Enum):
 
 
 class Vault:
+    vault: Dict[str, Dict]
+
     def __init__(self):
         if not os.path.exists(data_dir):
             os.mkdir(data_dir)
@@ -41,8 +45,8 @@ class Vault:
         else:
             self.load_bank()
 
-    def get_bank(self, member: Member, c_list: Union[List[Currency]]) -> MessageChain:
-        user_bank = self.get_bank_dict(member, c_list, chs=True)
+    def get_bank_msg(self, member: Member, c_list: List[Currency]) -> MessageChain:
+        user_bank = self.get_bank(member, c_list, chs=True)
         return MessageChain.create([
             At(member),
             Plain(text=" 您的余额为"),
@@ -50,36 +54,38 @@ class Vault:
             Plain(text=f" {value} {key}") for key, value in user_bank.items()
         ])
 
-    def get_bank_dict(self, member: Member, c_list: Union[List[Currency]], *, chs: bool = False) -> dict:
+    def get_bank(self, member: Member, c_list: Union[List[Currency]], *, chs: bool = False) -> Dict[str, int]:
         if member.id in self.vault.keys():
-            user_bank = {c.value[0 if not chs else 1]: self.vault[member.id].get(c.value[0]) for c in c_list}
+            user_bank = {c.value[0 if not chs else 1]: self.vault[str(member.id)].get(c.value[0]) for c in c_list}
         else:
             for c in c_list:
                 self.set_bank(member.id, c, 0)
-            user_bank = self.get_bank_dict(member, c_list, chs=chs)
+            user_bank = self.get_bank(member, c_list, chs=chs)
         return user_bank
 
-    def store_bank(self):
+    def store_bank(self) -> NoReturn:
         with open(vault_dir, "w", encoding="utf-8") as f:
             f.write(json.dumps(self.vault, indent=4))
 
-    def load_bank(self):
+    def load_bank(self) -> NoReturn:
         with open(vault_dir, "r", encoding="utf-8") as f:
             self.vault = json.loads(f.read())
 
-    def update_bank(self, supplicant: int, c: Currency, amount: int):
-        if supplicant in self.vault.keys():
-            self.vault[supplicant][c.value[0]] += amount
+    def update_bank(self, supplicant: int, c: Currency, amount: int) -> int:
+        if str(supplicant) in self.vault.keys():
+            self.vault[str(supplicant)][c.value[0]] += amount
         else:
-            self.vault[supplicant] = {[c.value[0]]: amount}
+            self.vault[str(supplicant)] = {[c.value[0]]: amount}
         self.store_bank()
+        return self.vault[str(supplicant)][c.value[0]]
 
-    def set_bank(self, supplicant: int, c: Currency, amount: int):
-        if supplicant in self.vault.keys():
-            self.vault[supplicant][c.value[0]] = amount
+    def set_bank(self, supplicant: int, c: Currency, amount: int) -> int:
+        if str(supplicant) in self.vault.keys():
+            self.vault[str(supplicant)][c.value[0]] = amount
         else:
-            self.vault[supplicant] = {c.value[0]: amount}
+            self.vault[str(supplicant)] = {c.value[0]: amount}
         self.store_bank()
+        return self.vault[str(supplicant)][c.value[0]]
 
 
 vault = Vault()
@@ -101,4 +107,53 @@ async def chitung_bank_handler(
         app: Ariadne,
         event: MessageEvent
 ):
-    await app.sendGroupMessage(event.sender.group, vault.get_bank(event.sender, [Currency.PUMPKIN_PESO]))
+    await app.sendGroupMessage(event.sender.group, vault.get_bank_msg(event.sender, [Currency.PUMPKIN_PESO]))
+
+
+@channel.use(
+    ListenerSchema(
+        listening_events=[GroupMessage],
+        inline_dispatchers=[
+            Twilight(
+                [
+                    FullMatch("/set"),
+                    RegexMatch(r"\d+ ") @ "target",
+                    RegexMatch(r"\d+") @ "amount"
+                ]
+            )
+        ]
+    )
+)
+async def chitung_bank_set_handler(
+        app: Ariadne,
+        event: MessageEvent,
+        target: RegexResult,
+        amount: RegexResult
+):
+    target = int(target.result.asDisplay().strip())
+    amount = int(amount.result.asDisplay())
+    vault.set_bank(target, Currency.PUMPKIN_PESO, amount)
+    await app.sendGroupMessage(event.sender.group, MessageChain("已设置成功。"))
+
+
+@channel.use(
+    ListenerSchema(
+        listening_events=[GroupMessage],
+        inline_dispatchers=[
+            Twilight(
+                [
+                    FullMatch("/laundry"),
+                    RegexMatch(r"\d+") @ "amount"
+                ]
+            )
+        ]
+    )
+)
+async def chitung_bank_laundry_handler(
+        event: MessageEvent,
+        amount: RegexResult
+):
+    if event.sender.id not in config.adminID:
+        return
+    amount = int(amount.result.asDisplay())
+    vault.update_bank(event.sender.id, Currency.PUMPKIN_PESO, amount)
