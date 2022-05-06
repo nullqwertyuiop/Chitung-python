6519711513c78a1247058522497e357c9cc96990
IshikawaKaito <435907629@qq.com>
2022-05-06 13:38:34 +0800

    [Feature] Add Blackjack

---
+++ chitung/blackjack/__init__.py
@@ -0,0 +1,346 @@
+import asyncio
+from pathlib import Path
+
+from graia.ariadne import Ariadne
+from graia.ariadne.event.message import GroupMessage, MessageEvent, FriendMessage
+from graia.ariadne.message.chain import MessageChain
+from graia.ariadne.message.element import Image, At
+from graia.ariadne.message.parser.twilight import Twilight, UnionMatch, MatchResult, RegexMatch
+from graia.saya import Channel
+from graia.saya.builtins.broadcast import ListenerSchema
+
+from .blackjack import BlackJackData, BlackJackPhase, Poker, PokerSuit
+from ..bank import vault, Currency
+from ..utils.depends import BlacklistControl, FunctionControl
+
+channel = Channel.current()
+
+channel.name("ChitungBlackJack")
+channel.author("角川烈&白门守望者 (Chitung-public)，IshikawaKaito (Chitung-python)")
+channel.description("七筒")
+
+
+@channel.use(
+    ListenerSchema(
+        listening_events=[GroupMessage, FriendMessage],
+        inline_dispatchers=[
+            Twilight(
+                [
+                    UnionMatch(
+                        "/deal", "/fold", "/split", "/double", "/pair", "/assurance", "/surrender",
+                        "要牌", "停牌", "分牌", "双倍下注", "下注对子", "买保险", "投降"
+                    ) @ "function",
+                ]
+            )
+        ],
+        decorators=[
+            BlacklistControl.enable(),
+            FunctionControl.enable("casino")
+        ]
+    )
+)
+async def chitung_blackjack_ops_handler(
+        app: Ariadne,
+        event: MessageEvent,
+        function: MatchResult
+):
+    game_id = event.sender.group.id if isinstance(event, GroupMessage) else event.sender.id
+    try:
+        if function.result.asDisplay() in ["/deal", "要牌"]:
+            await deal(app, event, game_id)
+        elif function.result.asDisplay() in ["/fold", "停牌"]:
+            await fold(app, event, game_id)
+        elif function.result.asDisplay() in ["/split", "分牌"]:
+            await split(app, event, game_id)
+        elif function.result.asDisplay() in ["/double", "双倍下注"]:
+            await double_bet(app, event, game_id)
+        elif function.result.asDisplay() in ["/pair", "下注对子"]:
+            await pair(app, event, game_id)
+        elif function.result.asDisplay() in ["/assurance", "买保险"]:
+            await assurance(app, event, game_id)
+        elif function.result.asDisplay() in ["/surrender", "投降"]:
+            await surrender(app, event, game_id)
+
+        bjd, player = get_valid_game_and_player(event, game_id)
+        if bjd.check_all_fold():
+            await checkout_game(app, event, game_id)
+    except ValueError:
+        return
+
+
+@channel.use(
+    ListenerSchema(
+        listening_events=[GroupMessage, FriendMessage],
+        inline_dispatchers=[
+            Twilight(
+                [
+                    RegexMatch(r"/bet\s*[0-9]*") @ "bet_command",
+                ]
+            )
+        ],
+        decorators=[
+            BlacklistControl.enable(),
+            FunctionControl.enable("casino")
+        ]
+    )
+)
+async def chitung_blackjack_bet_handler(
+        app: Ariadne,
+        event: MessageEvent,
+        bet_command: MatchResult,
+):
+    if isinstance(event, GroupMessage):
+        game_id = event.sender.group.id
+    else:
+        game_id = event.sender.id
+
+    bets = int(bet_command.result.asDisplay().replace("/bet", "").strip())
+    await bet(app, event, game_id, bets)
+
+
+@channel.use(
+    ListenerSchema(
+        listening_events=[GroupMessage, FriendMessage],
+        inline_dispatchers=[
+            Twilight(
+                [
+                    UnionMatch('/blackjack', '二十一点'),
+                ]
+            )
+        ],
+        decorators=[
+            BlacklistControl.enable(),
+            FunctionControl.enable("fish")
+        ]
+    )
+)
+async def chitung_blackjack_handler(
+        app: Ariadne,
+        event: MessageEvent,
+):
+    if isinstance(event, GroupMessage):
+        game_id = event.sender.group.id
+    else:
+        game_id = event.sender.id
+
+    if len([game for game in blackjack_game_data if game.id == game_id]) != 0:
+        await send_message(app, event, "游戏正在进行中。")
+    else:
+        reply_msg = "里格斯公司邀请您参与本局 Blackjack，请在60秒之内输入 /bet+数字 参与游戏。"
+        reply_msg += Image(path=assets_dir / "blackjack.png")
+        await send_message(app, event, reply_msg, False)
+        bjd = BlackJackData(game_id)
+        blackjack_game_data.append(bjd)
+        await asyncio.sleep(60)
+        if bjd.phase == BlackJackPhase.Callin:
+            blackjack_game_data.remove(bjd)
+            await send_message(app, event, "本局 Blackjack 已经取消。")
+        else:
+            return
+
+
+async def bet(app, event, game_id, bets):
+    bjd = get_game_data(game_id)
+    if bjd is None:
+        return
+    if bets <= 0:
+        raise ValueError
+
+    if purchase(event.sender, bets):
+        if bjd.phase == BlackJackPhase.Callin:
+            bjd.phase = BlackJackPhase.Bet
+            if event.sender.id == bjd.id:
+                asyncio.create_task(end_bet_phase(app, event, game_id, 0))
+            else:
+                await send_message(app, event, "Bet 阶段已经开始，预计在60秒之内结束。可以通过/bet+金额反复追加 bet。", False)
+                asyncio.create_task(end_bet_phase(app, event, game_id))
+        if bjd.get_player(event.sender.id) is None:
+            # 新玩家
+            prompt = f"已收到下注{bets}南瓜比索。"
+        else:
+            prompt = f"共收到下注{bets + bjd.get_player(event.sender.id).bet}南瓜比索。"
+        bjd.bet(event.sender.id, bets)
+        await send_message(app, event, prompt)
+    else:
+        await send_message(app, event, "操作失败，请检查您的南瓜比索数量。")
+
+
+async def end_bet_phase(app, event, game_id, time_out=60):
+    await asyncio.sleep(time_out)
+    bjd = get_game_data(game_id)
+    if bjd is None:
+        return
+    bjd.end_bet()
+    await send_message(app, event, "Bet 阶段已经结束。", at=False)
+    reply_msg = MessageChain.create("抽牌情况如下：\n")
+    reply_msg += f"庄家的牌是：\n{bjd.get_player(0).cards[0]} 暗牌"
+    for player in bjd.black_jack_players:
+        if player.player_id == 0:
+            continue
+        if event.sender.id != game_id:  # 判断是不是群
+            reply_msg += MessageChain.create("\n\n", At(player.player_id), " 的牌是：\n")
+        else:
+            reply_msg += MessageChain.create("\n\n你的牌是：\n")
+        reply_msg += f" {player.cards[0]} {player.cards[1]}"
+    await send_message(app, event, reply_msg, at=False)
+    await send_message(app, event, "现在可以进行操作，请在60秒之内完成。功能列表请参考说明书。", at=False)
+    await end_operate_phase(app, event, game_id)
+
+
+async def end_operate_phase(app, event, game_id):
+    await asyncio.sleep(60)
+    bjd = get_game_data(game_id)
+    if bjd is None:
+        # 提前结束
+        return
+    bjd.fold_all()
+    await checkout_game(app, event, game_id)
+
+
+async def checkout_game(app, event, game_id):
+    bjd = get_game_data(game_id)
+    # 庄家操作
+    bookmaker_pile_msg = bjd.bookmaker_operate()
+    await send_message(app, event, bookmaker_pile_msg, False)
+    reply_msg = MessageChain.create("本局游戏已经结束，里格斯公司感谢您的参与。如下为本局玩家获得的南瓜比索：\n")
+    result = bjd.check()
+    for p in bjd.black_jack_players:
+        if p.is_bookmaker:
+            continue
+        if event.sender.id == bjd.id:
+            reply_msg += f"\n您获得了{result[p.player_id]}南瓜比索。"
+        else:
+            reply_msg += MessageChain.create("\n", At(p.player_id), f" 获得了{result[p.player_id]}南瓜比索。")
+        exchange(p.player_id, result[p.player_id])
+    blackjack_game_data.remove(bjd)
+    await send_message(app, event, reply_msg, at=False)
+
+
+async def deal(app, event, game_id):
+    bjd, player = get_valid_game_and_player(event, game_id)
+    if player.can_operate:
+        deal_result = bjd.deal(event.sender.id)
+        await send_message(app, event, f"您抽到的牌是：{deal_result[0]}")
+        if not deal_result[1]:
+            await send_message(app, event, "您爆牌了。")
+            bjd.busted(player.player_id)
+
+
+async def fold(app, event, game_id):
+    bjd, player = get_valid_game_and_player(event, game_id)
+    if player.can_operate:
+        bjd.fold(player.player_id)
+        await send_message(app, event, "您已经停牌。")
+    if bjd.check_all_fold():
+        await checkout_game(app, event, game_id)
+
+
+async def split(app, event, game_id):
+    bjd, player = get_valid_game_and_player(event, game_id)
+    if not player.can_operate:
+        return
+    if not player.can_split():
+        return
+    if not purchase(event.sender, player.bet):
+        await send_message(app, event, "操作失败，请检查您的南瓜比索数量。")
+        return
+    raw_cards, piles = bjd.split(player.player_id)
+    reply_msg = MessageChain.create(f"您的原始牌堆为：\n{raw_cards[0]}{raw_cards[1]}\n您两个牌堆抽到的牌分别是：\n牌堆I：")
+    for card in piles[0]:
+        reply_msg += f" {card}"
+    reply_msg += "\n牌堆II："
+    for card in piles[1]:
+        reply_msg += f" {card}"
+    await send_message(app, event, reply_msg, game_id != event.sender.id)
+
+
+async def double_bet(app, event, game_id):
+    bjd, player = get_valid_game_and_player(event, game_id)
+    if not player.can_operate:
+        return
+    if not player.can_double_bet():
+        await send_message(app, event, "您的牌无法双倍下注。")
+        return
+    if not purchase(event.sender, player.bet):
+        await send_message(app, event, "操作失败，请检查您的南瓜比索数量。")
+        return
+    bjd.double_bet(player.player_id)
+
+
+async def assurance(app, event, game_id):
+    bjd, player = get_valid_game_and_player(event, game_id)
+    if not player.can_operate:
+        return
+    if bjd.assurance(player.player_id):
+        await send_message(app, event, "您已经购买保险。")
+    else:
+        await send_message(app, event, "目前的牌局无法购买保险。")
+
+
+async def pair(app, event, game_id):
+    bjd, player = get_valid_game_and_player(event, game_id)
+    if not player.can_operate:
+        return
+    if not purchase(event.sender, player.bet):
+        await send_message(app, event, "操作失败，请检查您的南瓜比索数量。")
+        return
+    if bjd.bet_pair(player.player_id):
+        await send_message(app, event, "您已经下注对子。")
+    else:
+        await send_message(app, event, "无法重复下注对子。")
+
+
+async def surrender(app, event, game_id):
+    bjd, player = get_valid_game_and_player(event, game_id)
+    if not player.can_operate:
+        return
+    bjd.surrender(player.player_id)
+    await send_message(app, event, "您投降了，将会返还您一半的赌注。")
+
+
+def get_valid_game_and_player(event, game_id):
+    bjd = get_game_data(game_id)
+    if bjd is None:
+        raise ValueError
+    player = bjd.get_player(event.sender.id)
+    if player is None:
+        raise ValueError
+    return bjd, player
+
+
+def purchase(sender, cost) -> bool:
+    if vault.has_enough_money(sender, Currency.PUMPKIN_PESO, cost):
+        vault.update_bank(sender.id, Currency.PUMPKIN_PESO, -cost)
+        return True
+    else:
+        return False
+
+
+def exchange(player_id, num):
+    vault.update_bank(player_id, Currency.PUMPKIN_PESO, num)
+
+
+async def send_message(app, event, msg, at=True):
+    if isinstance(event, GroupMessage):
+        if at:
+            msg = MessageChain.create(At(event.sender), msg)
+        await app.sendGroupMessage(
+            event.sender.group,
+            MessageChain.create(msg)
+        )
+    else:
+        await app.sendFriendMessage(
+            event.sender,
+            MessageChain.create(msg)
+        )
+
+
+def get_game_data(game_id):
+    res = [game for game in blackjack_game_data if game.id == game_id]
+    if len(res) != 1:
+        return None
+    return res[0]
+
+
+assets_dir = Path(Path(__file__).parent / "assets")
+blackjack_game_data: list[BlackJackData] = []
---
+++ chitung/blackjack/blackjack.py
@@ -0,0 +1,292 @@
+import random
+import time
+from enum import Enum
+from typing import List, Union
+
+DECK_NUM = 4  # 四副牌
+
+
+class BlackJackPhase(Enum):
+    Callin = 0
+    Bet = 1
+    Operation = 2
+
+
+class PokerSuit(Enum):
+    Diamond = "♦"
+    Club = "♣"
+    Heart = "♥"
+    Spade = "♠"
+
+    @staticmethod
+    def get(index: int):
+        return [PokerSuit.Diamond, PokerSuit.Club, PokerSuit.Heart, PokerSuit.Spade][index]
+
+
+class Poker:
+    _poker_name = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
+
+    def __init__(self, number: int, suit: PokerSuit):
+        self.number = number
+        self.suit = suit
+        self.point = min(number + 1, 10)
+
+    def __str__(self):
+        return str(self.suit.value) + self._poker_name[self.number]
+
+    def __eq__(self, other):
+        if not isinstance(other, Poker):
+            return False
+        return self.point == other.point and self.suit == other.suit
+
+    def __hash__(self):
+        return hash(self._poker_name)
+
+
+class BlackJackPlayer:
+
+    def __init__(self, player_id, bet=0, is_bookmaker=False):
+        self.player_id = player_id
+        self.is_bookmaker = is_bookmaker
+        self.bet = bet
+        self.cards: List[Poker] = []
+        self.bet_pair: bool = False
+        self.has_split: bool = False
+        self.can_operate: bool = False
+        self.is_double: bool = False
+        self.has_assurance: bool = False
+        self.has_busted: bool = False
+        self.has_surrendered: bool = False
+
+    def draw_card(self, cards: Union[Poker, list[Poker]]):
+        if isinstance(cards, Poker):
+            self.cards.append(cards)
+        elif isinstance(cards, list):
+            self.cards.extend(cards)
+
+    def calculate_point(self) -> int:
+        return self.calculate_cards_point(self.cards)
+
+    @staticmethod
+    def calculate_cards_point(cards: Union[list[Poker], Poker]):
+        point = 0
+        if isinstance(cards, Poker):
+            cards = [cards]
+        for card in sorted(cards, key=lambda x: -x.point):
+            if card.point == 1:
+                point += 11 if 11 + point <= 21 else 1
+            else:
+                point += card.point
+        return point
+
+    def can_double_bet(self) -> bool:
+        if len(self.cards) != 2:
+            return False
+        # 两张牌情况下21点只可能是A+10，这种情况必然可以看成11点
+        return self.calculate_point() in [11, 21] and not self.is_double
+
+    def can_split(self) -> bool:
+        if len(self.cards) != 2:
+            return False
+        return self.cards[0] == self.cards[1]
+
+
+class BlackJackData:
+    def __init__(self, game_id):
+        self.id = game_id
+        self.black_jack_players: list[BlackJackPlayer] = []
+        self.phase: BlackJackPhase = BlackJackPhase.Callin
+        self.card_number: int = 0
+        self.card_pile: list[Poker] = []
+        self.create_card_pile()
+        self.shuffle_card_pile()
+
+    def __eq__(self, other):
+        if isinstance(other, BlackJackData):
+            return False
+        return self.id == other.id
+
+    def __hash__(self):
+        return hash(self.id)
+
+    def create_card_pile(self):
+        for deck in range(DECK_NUM):
+            for i in range(52):
+                number = i // 4
+                suit = PokerSuit.get(i % 4)
+                card = Poker(number, suit)
+                self.card_pile.append(card)
+        self.shuffle_card_pile()
+
+    def shuffle_card_pile(self):
+        random.Random(time.time().real).shuffle(self.card_pile)
+
+    def bet(self, player_id, bet_num):
+        player = self.get_player(player_id)
+        if player not in self.black_jack_players:
+            self.black_jack_players.append(BlackJackPlayer(player_id, bet=bet_num))
+        else:
+            self.get_player(player_id).bet += bet_num
+
+    def end_bet(self):
+        """
+        结束下注，给所有下注玩家发两张牌
+        """
+        self.phase = BlackJackPhase.Operation
+        self.black_jack_players.append(BlackJackPlayer(0, 0, True))  # 添加一位庄家
+        for player in self.black_jack_players:
+            player.draw_card([self.card_pile.pop(), self.card_pile.pop()])
+        self.phase = BlackJackPhase.Operation
+        for p in self.black_jack_players:
+            p.can_operate = True
+
+    def get_player(self, player_id):
+        res = [p for p in self.black_jack_players if p.player_id == player_id]
+        if len(res) != 1:
+            return None
+        return res[0]
+
+    def deal(self, player_id):
+        """加牌，爆牌输出(抽到的牌，爆牌了吗)"""
+        player = self.get_player(player_id)
+        card = self.card_pile.pop()
+        player.draw_card(card)
+        return card, player.calculate_point() <= 21
+
+    def assurance(self, player_id):
+        """买保险"""
+        if self.get_player(0).cards[0].number == 0:  # A的序号是0
+            self.get_player(player_id).has_assurance = True
+            return True
+        return False
+
+    def double_bet(self, player_id):
+        """双倍下注"""
+        player = self.get_player(player_id)
+        player.bet *= 2
+        player.can_operate = False
+
+    def fold(self, player_id):
+        """停牌"""
+        self.get_player(player_id).can_operate = False
+
+    def bet_pair(self, player_id):
+        """下注对子"""
+        player = self.get_player(player_id)
+        if not player.bet_pair:
+            player.bet_pair = True
+            return True
+        return False
+
+    def split(self, player_id):
+        """分牌"""
+        player = self.get_player(player_id)
+        player.has_split = True
+        player.can_operate = False
+        piles = [[player.cards[0]], [player.cards[1]]]  # 分成两个牌堆
+        for pile in piles:
+            while player.calculate_cards_point(pile) < 17:
+                poker = self.card_pile.pop()
+                pile.append(poker)
+        raw_cards = player.cards
+        player.cards = piles
+        return raw_cards, piles
+
+    def surrender(self, player_id):
+        """投降"""
+        self.get_player(player_id).can_operate = False
+        self.get_player(player_id).has_surrendered = True
+
+    def busted(self, player_id):
+        player = self.get_player(player_id)
+        player.can_operate = False
+        player.has_busted = True
+
+    def check_all_fold(self):
+        return len(
+            [player for player in self.black_jack_players if player.player_id != 0 and player.can_operate]
+        ) == 0
+
+    def fold_all(self):
+        for p in self.black_jack_players:
+            self.fold(p.player_id)
+
+    def bookmaker_operate(self):
+        bookmaker = self.get_player(0)
+        while bookmaker.calculate_point() < 17:
+            self.deal(0)
+        msg = f"庄家开的牌组是：\n"
+        for card in bookmaker.cards:
+            msg += f" {card}"
+        return msg
+
+    def check(self):
+        result = {}
+        for p in self.black_jack_players:
+            if p.is_bookmaker:
+                continue
+            result[p.player_id] = self.calculate_bet(p.player_id)
+        return result
+
+    def calculate_bet(self, player_id):
+        """计算赌局结果"""
+        player = self.get_player(player_id)
+        coefficient = ((1 - int(player.has_assurance) * 0.5) * self.normal_point(player_id) +
+                       self.pair_point(player_id) +
+                       self.special_pattern_point(player_id) +
+                       int(player.has_assurance) * self.assurance_point(player_id)
+                       )
+        return player.bet * coefficient
+
+    def normal_point(self, player_id):
+        """计算得分"""
+        player = self.get_player(player_id)
+        if player.has_surrendered:
+            return 0.5
+        if player.has_split:
+            point_1 = player.calculate_cards_point(player.cards[0])
+            if point_1 > 21:
+                point_1 = 0
+            point_2 = player.calculate_cards_point(player.cards[1])
+            if point_2 > 21:
+                point_2 = 0
+            return self.compare_with_bookmaker(point_1) + self.compare_with_bookmaker(point_2)
+        if player.has_busted:
+            return 0
+        return self.compare_with_bookmaker(player.calculate_point())
+
+    def pair_point(self, player_id):
+        player = self.get_player(player_id)
+        bookmaker = self.get_player(0)
+        if not player.bet_pair:
+            return 0
+        return 11 if bookmaker.cards[0] == bookmaker.cards[1] else 0
+
+    def assurance_point(self, player_id):
+        player = self.get_player(player_id)
+        bookmaker = self.get_player(0)
+        if not player.has_assurance:
+            return 0
+        return int(bookmaker.cards[1] == 10) * (1 + int(player.has_split))
+
+    def special_pattern_point(self, player_id):
+        player = self.get_player(player_id)
+        cards = player.cards
+        if len(cards) != 3:
+            return 0
+        cards_point = [card.point for card in cards]
+        if all(_ == 7 for _ in cards_point) or {6, 7, 8}.issubset(set(cards_point)):
+            return 3
+        return 0
+
+    def compare_with_bookmaker(self, point):
+        bookmaker = self.get_player(0)
+        bookmaker_point = bookmaker.calculate_point()
+        if bookmaker_point > 21:
+            bookmaker_point = 1
+        if bookmaker_point > point:
+            return 0
+        elif bookmaker_point == point:
+            return 1
+        else:
+            return 2
