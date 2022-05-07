f2f3d6a281304f7c3c6996efa8276abb12ae6c98
nullqwertyuiop <49110142+nullqwertyuiop@users.noreply.github.com>
2022-05-07 23:00:09 +0800

    [Fix] Minor patches
    1. Fixed spacing
    2. Use `setattr()` instead of `exec()` [Source](https://github.com/nullqwertyuiop/Chitung-python/commit/7c78f254db88b781065eea79048535b2f1840d2b#r73098586)

--- chitung/utils/__init__.py
+++ chitung/utils/__init__.py
@@ -3,7 +3,9 @@ from graia.ariadne.event.lifecycle import ApplicationLaunched
 from graia.ariadne.event.message import GroupMessage, MessageEvent, FriendMessage
 from graia.ariadne.exception import UnknownTarget, AccountMuted
 from graia.ariadne.message.chain import MessageChain
-from graia.ariadne.message.parser.twilight import Twilight, UnionMatch, MatchResult, FullMatch, RegexMatch, RegexResult
+from graia.ariadne.message.parser.twilight import (Twilight, UnionMatch, MatchResult,
+                                                   FullMatch, RegexMatch, RegexResult,
+                                                   SpacePolicy)
 from graia.saya import Channel
 from graia.saya.builtins.broadcast import ListenerSchema
 
@@ -14,7 +16,7 @@ from ..utils.depends import BlacklistControl, Permission
 channel = Channel.current()
 
 channel.name("ChitungAdminTools")
-channel.author("角川烈&白门守望者 (Chitung-public), nullqwertyuiop (Chitung-python)")
+channel.author("角川烈&白门守望者 (Chitung-public), IshikawaKaito&nullqwertyuiop (Chitung-python)")
 channel.description("七筒")
 
 
@@ -60,9 +62,9 @@ async def chitung_admin_help_handler(
                      # "Blacklist：\n"
                      # "/block 空格 -g 或者 -f 空格 QQ号：屏蔽该号码的群聊或者用户\n"
                      # "/unblock 空格 -g 或者 -f 空格 QQ号：解除屏蔽该号码的群聊或者用户\n\n"
-                     # "Config：\n"
-                     # "/config -h：查看 config 的帮助\n"
-                     # "/config 空格 数字序号 空格 true/false：开关相应配置\n\n"
+                     "Config：\n"
+                     "/config -h：查看 config 的帮助\n"
+                     "/config 空格 数字序号 空格 true/false：开关相应配置\n\n"
                      "Data：\n"
                      "/num -f：查看好友数量\n"
                      "/num -g：查看群聊数量\n"
@@ -248,7 +250,7 @@ async def chitung_reset_config_handler(
         inline_dispatchers=[
             Twilight(
                 [
-                    FullMatch("/"),
+                    FullMatch("/").space(SpacePolicy.NOSPACE),
                     UnionMatch("open", "close") @ "option",
                     UnionMatch("global", "fish", "casino", "responder", "game", "lottery", optional=True) @ "funct"
                 ]
@@ -267,19 +269,26 @@ async def chitung_group_config_handler(
         funct: MatchResult
 ):
     funct_type = {
-        "global": "全局消息", "fish": "钓鱼", "casino": "娱乐游戏", "responder": "关键词触发功能", "game": "所有游戏",
+        "global": "全局消息",
+        "fish": "钓鱼",
+        "casino": "娱乐游戏",
+        "responder": "关键词触发功能",
+        "game": "所有游戏",
         "lottery": "C4和Bummer功能"
     }
     if not funct.matched:
         await app.sendGroupMessage(
             event.sender.group,
-            MessageChain("群设置指示词使用错误，使用/close或者/open加上空格加上global、game、casino、responder、fish或者lottery来开关相应内容。")
+            MessageChain("群设置指示词使用错误，"
+                         "使用/close或者/open加上空格加上"
+                         "global、game、casino、responder、fish"
+                         "或者lottery来开关相应内容。")
         )
         return
     funct = funct.result.asDisplay()
     value = option.result.asDisplay() == "open"
     gc = group_config.get(event.sender.group.id)
-    exec(f"gc.{funct.replace('global', 'globalControl')} = {value}")
+    setattr(gc, funct.replace('global', 'globalControl'), value)
     save_group_config()
     await app.sendGroupMessage(
         event.sender.group,
