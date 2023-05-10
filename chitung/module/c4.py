09a395dddacef718b87380dbd3ea64f8e3ea8e62
nullqwertyuiop <null@member.fsf.org>
2023-05-10 20:19:54 +0800

    :bug: Fixed permission check
    Signed-off-by: nullqwertyuiop <null@member.fsf.org>

--- chitung/module/c4.py
+++ chitung/module/c4.py
@@ -25,7 +25,7 @@ class _C4FlagStore:
     Switch.check(GroupMessage, FunctionType.LOTTERY),
 )
 async def c4_handler(client: Client, group: Group, member: Member):
-    if (await client.get_member(group.uin, client.uin)).permission == 0:
+    if (await client.get_member(group.uin, client.uin)).permission not in (1, 2):
         return await client.send_group_message(
             group.uin, MessageChain([Text("七筒目前还没有管理员权限，请授予七筒权限解锁更多功能。")])
         )
